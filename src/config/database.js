6bf9218cc4f7d1ab6d39ce10c8393293e232a694
Gabriel G <125676394+gabrielguas@users.noreply.github.com>
2024-02-12 22:50:40 -0300

    cambios

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { MONGODB_URI } = process.env;
 
 const mongoDBConnection = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://guasgabriel22:FanoQ6mSsi7a1iwu@curso-backend-ch.j0ycecz.mongodb.net/entregaDB?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    );
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log("MongoDB connected!");
   } catch (error) {
     console.error("MongoDB connection error:", error);
